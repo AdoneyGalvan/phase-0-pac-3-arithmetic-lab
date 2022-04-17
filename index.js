1350915dc4a26b6cbbf85ed792cec73a0657c1b9
Aron Galvan <arongalvan03@gmail.com>
2022-04-17 15:05:19 -0700

    commit tree 654a52bd74fb47d5c0c3eef3502f579768d8920e

---
+++ index.js
@@ -0,0 +1,39 @@
+const add = (a, b) => 
+{
+    return a + b;
+}
+
+const subtract = (a, b) => 
+{
+    return a - b;
+}
+
+const divide = (a, b) => 
+{
+    return a / b;
+}
+
+const multiply = (a, b) => 
+{
+    return a * b;
+}
+
+const increment = (a) => 
+{
+    return a + 1;
+}
+
+const decrement = (a) => 
+{
+    return a - 1;
+}
+
+const makeInt = (n) => 
+{
+    return parseInt(n, 10);
+}
+
+const preserveDecimal = (n) => 
+{
+    return parseFloat(n);
+}
